017035ec06c121e7ace21bbc8e11c1af61893323
GenkunAbe <genkunabe@gmail.com>
2018-05-07 22:52:41 +0800

    fix mean & std

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ transform = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     transforms.Resize((384, 128)),
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    transforms.Normalize((0.486, 0.459, 0.408), (0.229, 0.224, 0.225))
 ])
