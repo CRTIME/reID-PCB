7fcd6debf834d03f26034aebc7b4d06ee4b0b286
GenkunAbe <genkunabe@gmail.com>
2018-05-07 22:21:50 +0800

    fix resolution

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import torchvision.transforms as transforms
 
 transform = transforms.Compose([
     transforms.RandomHorizontalFlip(),
-    transforms.Resize((768, 256)),
-    transforms.ToTensor(), 
+    transforms.Resize((384, 128)),
+    transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
